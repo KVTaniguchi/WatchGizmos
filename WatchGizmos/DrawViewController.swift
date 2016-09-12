d8f74610e18ad1ee05c41810e710c33f8c3a1fbd
Kevin Taniguchi <kevintaniguchi@Kevins-MacBook-Pro-2.local>
2016-09-11 23:12:23 -0500

    added width slider

--- WatchGizmos/DrawViewController.swift
+++ WatchGizmos/DrawViewController.swift
@@ -16,6 +16,7 @@ class DrawViewController: UIViewController {
     let penButton = UIButton()
     let widthButton = UIButton()
     let drawingView = ACEDrawingView()
+    let widthSlider = UISlider()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +26,27 @@ class DrawViewController: UIViewController {
         view.addSubview(drawingView)
         
         setUpButtons()
+        setUpSlider()
     }
 }
 
 extension DrawViewController {
+    func setUpSlider() {
+        widthSlider.maximumValue = 20
+        widthSlider.minimumValue = 1
+        widthSlider.value = 10
+        widthSlider.isHidden = true
+        widthSlider.translatesAutoresizingMaskIntoConstraints = false
+        drawingView.addSubview(widthSlider)
+        widthSlider.bottomAnchor.constraint(equalTo: penButton.topAnchor, constant: -15).isActive = true
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-10-[widthSlider]-10-|", options: [], metrics: nil, views: ["widthSlider": widthSlider]))
+    }
+    
     func setUpButtons() {
         undoButton.setTitle(NSLocalizedString("Undo", comment: ""), for: .normal)
         redoButton.setTitle(NSLocalizedString("Redo", comment: ""), for: .normal)
         clearButton.setTitle(NSLocalizedString("Clear", comment: ""), for: .normal)
-        penButton.setTitle(NSLocalizedString("Pen", comment: ""), for: .normal)
+        penButton.setTitle(NSLocalizedString("Pen + Text", comment: ""), for: .normal)
         widthButton.setTitle(NSLocalizedString("Width", comment: ""), for: .normal)
         
         undoButton.addTarget(self, action: #selector(undoButtonPressed), for: .touchUpInside)
@@ -51,23 +64,23 @@ extension DrawViewController {
         }
         
         undoButton.topAnchor.constraint(equalTo: drawingView.topAnchor, constant: 66).isActive = true
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-60-[undoButton(clearButton)]-40-[redoButton(clearButton)]-40-[clearButton]", options: [.alignAllTop, .alignAllBottom], metrics: nil, views: buttons))
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[undoButton(clearButton)]-40-[redoButton(clearButton)]-40-[clearButton]", options: [.alignAllTop, .alignAllBottom], metrics: nil, views: buttons))
         penButton.bottomAnchor.constraint(equalTo: drawingView.bottomAnchor, constant: -15).isActive = true
-        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-60-[penButton]-40-[widthButton(penButton)]", options: [.alignAllTop, .alignAllBottom], metrics: nil, views: buttons))
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[penButton]-40-[widthButton(penButton)]", options: [.alignAllTop, .alignAllBottom], metrics: nil, views: buttons))
     }
 }
 
 extension DrawViewController {
     func undoButtonPressed() {
-        
+        drawingView.undoLatestStep()
     }
     
     func redoButtonPressed() {
-        
+        drawingView.redoLatestStep()
     }
     
     func clearButtonPressed() {
-        
+        drawingView.clear()
     }
     
     func penButtonPressed() {
@@ -75,6 +88,24 @@ extension DrawViewController {
     }
     
     func widthButtonPressed() {
-        
+        widthSlider.isHidden = !widthSlider.isHidden
     }
 }
+
+//- (IBAction)undo:(id)sender
+//{
+//    [self.drawingView undoLatestStep];
+//    [self updateButtonStatus];
+//    }
+//    
+//    - (IBAction)redo:(id)sender
+//{
+//    [self.drawingView redoLatestStep];
+//    [self updateButtonStatus];
+//    }
+//    
+//    - (IBAction)clear:(id)sender
+//{
+//    [self.drawingView clear];
+//    [self updateButtonStatus];
+//}
